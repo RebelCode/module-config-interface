a5e3d360f63f89deeb8d4b327c2702bb88601692
Anton Ukhanev <xedin.unknown@gmail.com>
2017-06-09 01:23:59 +0200

    Added test

---
+++ test/unit/ConfigInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace RebelCode\Modular\UnitTest;
+
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \RebelCode\Modular\ConfigInterface}.
+ */
+class ConfigInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\\Modular\\ConfigInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return ModuleInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'Subject is not a valid instance.'
+        );
+    }
+}
